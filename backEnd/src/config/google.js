5d45b6fdc162e3618fa59705513d153899aee958
Daky Males <teomc0087@gmail.com>
2026-03-30 23:48:47 -0500

    cambio en credenciales de google

--- backEnd/src/config/google.js
+++ backEnd/src/config/google.js
@@ -1,27 +1,12 @@
-//uso de google CALENDARimport { google } from 'googleapis';
-/*
--autentica tu backend con Google
-
--usa el calendario de TU cuenta
-
--permite crear / borrar eventos
-*/
 import { google } from 'googleapis';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-// esto es solo para que Node sepa dónde está el JSON
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const credentials = JSON.parse(process.env.GOOGLE_KEY_JSON);
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, 'google-key.json'),
-  scopes: ['https://www.googleapis.com/auth/calendar']
+  credentials,
+  scopes: ['https://www.googleapis.com/auth/calendar'],
 });
 
-const calendar = google.calendar({
-  version: 'v3',
-  auth
-});
+const calendar = google.calendar({ version: 'v3', auth });
 
 export default calendar;
