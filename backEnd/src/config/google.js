456a85c6b92de0878d881f586f842e7bc78d293f
Daky Males <teomc0087@gmail.com>
2026-04-06 23:24:24 -0500

    cambiosFinales

--- backEnd/src/config/google.js
+++ backEnd/src/config/google.js
@@ -1,12 +1,10 @@
 import { google } from 'googleapis';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const credentials = JSON.parse(process.env.GOOGLE_KEY_JSON);
+credentials.private_key = credentials.private_key.replace(/\\n/g, '\n');
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, 'google-key.json'),
+  credentials,
   scopes: ['https://www.googleapis.com/auth/calendar'],
 });
 
